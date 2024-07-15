21ffe214889dc7723ba9b808ca9922b18bb130b4
Miguel Gs <miguelgsantos98@gmail.com>
2024-07-15 15:50:21 -0300

    add: dotenv

--- src/database/conectaBanco.js
+++ src/database/conectaBanco.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 import "dotenv/config";
 
 export async function conectaBanco(){
-    const url = "mongodb+srv://miguelgsantos98:JV3J2emAdg02ZTo4@cluster0.eidghq8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+    const url = process.env.DATABASE_URI;
 
     try{
         await mongoose.connect(url);
