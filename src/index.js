973a409788899fc929a34f5c50bf209a903a3ba1
Miguel Gs <miguelgsantos98@gmail.com>
2024-07-23 21:56:09 -0300

    add: route transacoes / schema

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import express, {json} from 'express';
 import {conectaBanco} from './database/conectaBanco.js';
 import usuarioRoute from './routes/usuarioRoute.js';
+import transacoesRoute from './routes/transacoesRoute.js';
 
 const port = process.env.PORT;
 const app = express();
 
 app.use(json());
-
 app.use(usuarioRoute);
+app.use(transacoesRoute)
 
 conectaBanco();
 app.listen(port, ()=> console.log("Ouvindo a entradinha hmmm hehe!!"));
---
+++ src/schema/Transacoes.js
@@ -0,0 +1,12 @@
+
+import { Schema } from "mongoose";
+
+const TransacoesSchema = new Schema({
+    valor:{type: Number, required: true},
+    descricao:{type: String, required: true},
+    tipo:{type: String, required: true},
+    usuarioId:{ type:Schema.Types.ObjectId, required: true, ref : "Usuarios"},
+    created_at:{type: Date, required: true, default: Date.now}
+});
+
+export default model ("transacoes", TransacoesSchema)
