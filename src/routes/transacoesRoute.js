7386bd610e43c09d32c0ead6005e08103322c94b
Miguel Gs <miguelgsantos98@gmail.com>
2024-07-24 00:10:05 -0300

    Add : validate transacoes

--- src/routes/transacoesRoute.js
+++ src/routes/transacoesRoute.js
@@ -1,13 +1,15 @@
 import { Router } from "express";
 import transacoesControle from "../controller/transacoesControle.js"
 import autenticacaoMiddleware from "../middleware/autenticacaoMiddleware.js";
+import { validacaoMiddleware } from "../middleware/validacaoMiddleware.js";
+import { validaTransacoes } from "../schema/validation/validaTransacoes.js";
 
 const transacoesRoute = Router();
 
 transacoesRoute.use(autenticacaoMiddleware)
 
 
-transacoesRoute.post("/",  transacoesControle.create);
+transacoesRoute.post("/", validacaoMiddleware(validaTransacoes), transacoesControle.create);
 transacoesRoute.get("/",  transacoesControle.findByUsuario);
 
 export default transacoesRoute;
---
+++ src/schema/validation/validaTransacoes.js
@@ -0,0 +1,18 @@
+import Joi from 'joi';
+
+export const validaTransacoes = Joi.object({
+    valor: Joi.number().required().messages({
+        'number.base': 'O campo valor deve ser um número.',
+        'any.required': 'O campo valor é obrigatório.'
+    }),
+    descricao: Joi.string().required().messages({
+        'string.base': 'O campo descricao deve ser uma string.',
+        'any.required': 'O campo descricao é obrigatório.'
+    }),
+    tipo: Joi.string().required().valid("input", "output").messages({
+        'string.base': 'O campo tipo deve ser uma string.',
+        'any.required': 'O campo tipo é obrigatório.'
+    }),
+    userId: Joi.object(),
+    createdAt: Joi.date()
+});
